3c780293eef5a1e016c188ad0cc87de87f3b69e9
Jill Hubbard <jill.m.hubbard@gmail.com>
2023-03-07 23:50:08 -0800

    update graph.py header

--- graph.py
+++ graph.py
@@ -1,8 +1,10 @@
 # Graph: An efficient graph.
-# A graph implementation that uses an adjacency list to represent vertices
-# and edges.
+# A graph implementation that uses an adjacency list to represent vertices and edges.
 # Your implementation should pass the tests in test_graph.py.
+
 # YOUR NAME
+# YOUR COLLABORATORS
+# TIME SPENT: 
 
 import functools
 
